bdd306084c98849f6205811e8c5154749cb91099
blitheli <blitheli@gmail.com>
2023-06-20 19:11:43 +0800

    完善createConfig

--- createConfig.py
+++ createConfig.py
@@ -1,16 +1,31 @@
-# read config.json from all sub directories and return json object
+#   读取当前目录下所有文件夹内的config.json文件，生成特定的json对象（放在Layers数组中），并保存为文件
+#   2023-06-20  liyunfei
+#
 import os
 import json
+import datetime
 
 def read_config():
 
-    config_data = {}
+    config_data = {"Description":"本文件存储了瓦片服务器发布的相关图层信息",
+                   "version":"1.0.0",
+                   "Author":"liyunfei",
+                   "Date": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                   "Layers": []}
+    
+    # 循环每个文件夹 
     for root, dirs, files in os.walk(".", topdown=True):
+        # 循环每个文件
         for name in files:
             if name == "config.json":
                 file_path = os.path.join(root, name)
-                with open(file_path, "r") as config_file:
-                    config_data = json.load(config_file)
+                with open(file_path, "r", encoding='utf-8') as config_file:
+                    config_data['Layers'].append(json.load(config_file))
+
     return config_data
 
-print(read_config())
+jsonConfig = read_config()
+with open("data.json", "w", encoding='utf-8') as file:
+    json.dump(jsonConfig, file, ensure_ascii=False) # 保证中文不乱码
+
+print("----------end----------")
